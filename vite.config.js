3c2b7a7ec36a9f4112b5dbda5aa37069cb4af1f8
elhanafi <elhanafi@localhost.localdomain>
2025-08-03 16:35:55 +0000

    Update project for new repository name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Add this 'base' property:
-  base: '/bbdbd/', // <-- IMPORTANT: Replace 'rihla-vision' with your repository name
+  base: '/Rihla-Donations-journey/', // <-- IMPORTANT: Replace 'rihla-vision' with your repository name
   plugins: [react()],
 })
