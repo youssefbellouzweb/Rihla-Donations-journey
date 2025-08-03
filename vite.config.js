7890dba24d5b16b17f36aee6ab1ebfa4dcf3700c
elhanafi <elhanafi@localhost.localdomain>
2025-08-03 16:06:39 +0000

    Configure vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add this 'base' property:
+  base: '/bbdbd/', // <-- IMPORTANT: Replace 'rihla-vision' with your repository name
   plugins: [react()],
 })
