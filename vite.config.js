f1a13adb618b56c964f3daf5b860d0fca96408bc
youssefbellouzweb <youssefbellouzweb@gmail.com>
2025-08-11 23:11:25 +0100

    change url path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // This line is the entire fix.
   // It tells your website its address includes '/Rihla-Donations-journey/'.
-  base: '/Rihla-Donations-journey/',
+  base: '/',
   
   plugins: [react()],
 })
