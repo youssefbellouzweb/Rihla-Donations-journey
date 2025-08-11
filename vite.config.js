d72a6a49dedf628e6587e4ea02ac10619d62c23a
elhanafi <elhanafi@localhost.localdomain>
2025-08-11 20:12:54 +0000

    Update project for new photos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this 'base' property:
-  base: '/Rihla-Donations-journey/', // <-- IMPORTANT: Replace 'rihla-vision' with your repository name
+  // This tells your website where to find all your files
+  base: '/Rihla-Donations-journey/',
+  
   plugins: [react()],
 })
